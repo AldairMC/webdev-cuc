ed1a595097a3a86f8583dfcb1bd45676b7da2058
AldairMC <aldairmc2023@gmail.com>
2019-05-04 18:41:09 +0000

    06-04-19

---
+++ 6-04-19/app.js
@@ -0,0 +1,54 @@
+let pais = [
+    {
+        codigo : 1,
+        nombre : 'Colombia'
+    }
+];
+
+let departamento = [
+    {
+        codigo: '0001',
+        nombre: 'Atlantico',
+        pais: 1
+    },
+    {
+        codigo: '0002',
+        nombre: 'Magdalena',
+        pais: 1
+    },
+    {
+        codigo: '0003',
+        nombre: 'Cesar',
+        pais: 1
+    }
+];
+
+let ciudad = [
+    {
+        codigo: '0001-1',
+        nombre: 'Barranquilla',
+        departamento: '0001',
+        pais: 1
+    },
+
+    {
+        codigo: '0001-2',
+        nombre: 'Pivijay',
+        departamento: '0002',
+        pais: 1
+    },
+
+    {
+        codigo: '0001-3',
+        nombre: 'valledupar',
+        departamento: '0003',
+        pais: 1
+    },
+
+    {
+        codigo: '0001-4',
+        nombre: 'santa marta',
+        departamento: '0002',
+        pais: 1
+    }
+];
